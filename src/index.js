8b2aef209169356f5a0efa9c35c0608ce29a48ac
Aditya kumar sahu <135954802+adityakumarsahu108@users.noreply.github.com>
2026-08-21 00:11:02 +0530

    Remove extra placeholders in SQL VALUES clause

--- src/index.js
+++ src/index.js
@@ -301,7 +301,7 @@ async function handleCreateReport(
                 VALUES (
                     ?, ?, ?, ?, ?, ?, ?, ?,
                     ?, ?, ?, ?, ?, ?, ?, ?,
-                    ?, ?, ?, ?, ?, ?, ?, ?
+                    ?, ?, ?, ?, ?, ?, ?
                 )
             `)
             .bind(
